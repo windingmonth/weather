5cdc51a746668f4ae1ae2caf2f427b1194c9cf80
Eric <Eric@fts.dev>
2023-04-14 16:37:47 +0800

    feat

---
+++ Api/Promisedemo.js
@@ -0,0 +1,23 @@
+function test(resolve, reject) {
+    var timeOut = Math.random() * 2;
+    log('set timeout to: ' + timeOut + ' seconds.');
+    setTimeout(function () {
+        if (timeOut < 1) {
+            log('call resolve()...');
+            resolve('200 OK');
+        }
+        else {
+            log('call reject()...');
+            reject('timeout in ' + timeOut + ' seconds.');
+        }
+    }, timeOut * 1000);
+}
+
+
+new Promise(test).then(function(result){
+    console.log('成功：' + result);
+}).catch(function(error){
+    console.log('失败：' + error);
+})
+
+
